1f04334a6aba03abe5b185f7198358ea22dfe300
Ondřej Čížek <ondrej.cizek@giant.cz>
2023-07-06 22:57:15 +0200

    Svelte Config Fix

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,27 +1,28 @@
-
-
 <header class="header">
-    <div class="header-layout">
-        <h3 class="header-heading">
-           <!--  <img src="img/worker-fusko.png" alt=""> -->
-            Zemní práce Augustin Fusko
-        </h3>
-        <div class="hamburger">
-            <span class="stick1 stick"></span>
-            <span class="stick2 stick"></span>
-            <span class="stick3 stick"></span>
-        </div>
-    </div>
-    <nav class="nav">
-        <ul class="nav-list">
-            <li><a class="nav-item" href="#">Hlavní stránka</a></li>
-            <li><a class="nav-item" href="#">Ceník</a></li>
-            <li><a class="nav-item" href="#">Kontakt</a></li>
-        </ul>
-    </nav>
+	<div class="header-layout">
+		<h3 class="header-heading">
+			<!--  <img src="img/worker-fusko.png" alt=""> -->
+			Zemní práce Augustin Fusko
+		</h3>
+		<div class="hamburger">
+			<span class="stick1 stick" />
+			<span class="stick2 stick" />
+			<span class="stick3 stick" />
+		</div>
+	</div>
+	<nav class="nav">
+		<ul class="nav-list">
+			<li><a class="nav-item" href="#todo">Hlavní stránka</a></li>
+			<li><a class="nav-item" href="#todo">Ceník</a></li>
+			<li><a class="nav-item" href="#todo">Kontakt</a></li>
+		</ul>
+	</nav>
 </header>
 
 <style lang="stylus">
     .header
         background-color red
+
+        a
+            color black
 </style>
